9ddd9d8dff95b01da6d8e12ac54a2b53e6d35124
nandini <nandusolanki2005@gmail.com>
2025-08-25 12:09:28 +0530

    odd/even

---
+++ odd_even.py
@@ -0,0 +1,23 @@
+num=int(input("enter yhe number:"))
+rem=num%2
+if (rem==0):
+  print("even")
+else:
+  print("odd")
+
+no1 = 56
+no2 = 450
+no3 = 200
+if(no1 > no2 and no1 > no3):
+    print(f"no1:{no1}")
+elif(no2 > no3):
+    print(f"no2:{no2}")
+else:
+    print(f"no3 :{no3}")
+
+x = int(input("enter the value:"))
+
+if(x % 7 ==0):
+  print("yes")
+else:
+  print("no devided by 7")
